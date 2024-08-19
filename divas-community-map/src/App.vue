8c1980d57da0c42d61de5b347a05c9d9b4fa3c18
mashiyatz <mashiyat.zaman@gmail.com>
2024-08-19 06:39:31 -0400

    add markers

--- divas-community-map/src/App.vue
+++ divas-community-map/src/App.vue
@@ -15,11 +15,29 @@
 <script lang="ts">
 // Following this example: https://maplibre.org/maplibre-gl-js/docs/examples/restrict-bounds/
 
-import { Map, type LngLatBoundsLike } from 'maplibre-gl'
+import { Map, MapMouseEvent, type LngLatBoundsLike } from 'maplibre-gl'
 
 const bounds: LngLatBoundsLike | undefined = [
-  [-74.04728500751165, 40.68392799015035], // Southwest coordinates
-  [-73.91058699000139, 40.87764500765852] // Northeast coordinates
+  [-74.050696, 40.547044], // Southwest coordinates
+  [-73.817894, 40.721256] // Northeast coordinates
+]
+
+const landmarks = [
+  {
+    url: 'https://api.maptiler.com/maps/streets/style.json?key=vbWcEeVNDHjFUuEi2uGd', 
+    id: 'Place 1', 
+    coords: [-73.9978, 40.7209]
+  }, 
+  {
+    url: 'https://api.maptiler.com/maps/streets/style.json?key=vbWcEeVNDHjFUuEi2uGd', 
+    id: 'Place 2', 
+    coords: [-73.9978, 40.7209]
+  },
+  {
+    url: 'https://api.maptiler.com/maps/streets/style.json?key=vbWcEeVNDHjFUuEi2uGd', 
+    id: 'Place 3', 
+    coords: [-73.9978, 40.7209]
+  }
 ]
 
 export default {
@@ -27,13 +45,85 @@ export default {
     const map: Map = new Map({
       container: 'map',
       style: 'https://api.maptiler.com/maps/streets/style.json?key=vbWcEeVNDHjFUuEi2uGd',
-      // style: 'https://demotiles.maplibre.org/style.json',
       center: [-73.9978, 40.7209],
-      zoom: 14,
+      pitch: 60,
+      minPitch: 60,
+      maxPitch: 60,
+      zoom: 12,
       maxBounds: bounds
     })
-  }
-}
+
+    map.on('load', async () => {
+        var image = await map.loadImage("https://upload.wikimedia.org/wikipedia/commons/7/74/Location_icon_from_Noun_Project.png");
+        map.addImage("marker", image.data);
+        // See: https://maplibre.org/maplibre-gl-js/docs/API/classes/Map/#addsource
+        // See: https://maplibre.org/maplibre-style-spec/sources/#image
+        // See: https://maplibre.org/maplibre-gl-js/docs/examples/multiple-geometries/
+        // Example: https://jsfiddle.net/oh8Ld1ry/1/
+        // Discussion: https://github.com/mapbox/mapbox-gl-js/issues/4736
+        // Discussion: https://stackoverflow.com/questions/66210679/mapbox-gl-js-how-to-add-multiple-photo-sources-on-a-single-layer
+        map.addSource('point', { // creating a new type of source
+              'type': 'geojson',
+              'data': {
+                  'type': 'FeatureCollection',
+                  'features': [
+                      {
+                        'type': 'Feature',
+                        "properties": {},
+                        'geometry': {
+                          'type': 'Point',
+                          'coordinates': [-73.97549301444425, 40.674121965974805]
+                        }
+                      },
+                      {
+                        'type': 'Feature',
+                        "properties": {},
+                        'geometry': {
+                          'type': 'Point',
+                          'coordinates': [-73.94201080976683, 40.63684839590867]
+                        }
+                      },
+                      {
+                        'type': 'Feature',
+                        "properties": {},
+                        'geometry': {
+                          'type': 'Point',
+                          'coordinates': [-73.9978, 40.7209]
+                        }
+                      }
+                  ]
+              }
+          });
+        
+          // want to keep images in a single layer
+          map.addLayer({
+            'id': 'points',
+            'type': 'symbol',
+            'source': 'point',
+            'layout': {
+              'icon-image': 'marker',
+              'icon-size': 0.1
+            }
+          })
+
+          map.on('click', 'points', (e: any) => {
+            map.flyTo({
+                center: e.features[0].geometry.coordinates
+            });
+          });
+
+          // Change the cursor to a pointer when the it enters a feature in the 'symbols' layer.
+          map.on('mouseenter', 'points', () => {
+              map.getCanvas().style.cursor = 'pointer';
+          });
+
+          // Change it back to a pointer when it leaves.
+          map.on('mouseleave', 'points', () => {
+              map.getCanvas().style.cursor = '';
+          });
+        }
+      )}
+    }
 </script>
 
 <style lang="scss" scoped>
